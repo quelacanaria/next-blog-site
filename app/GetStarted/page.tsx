521f8f23d528334c2d459961c8ce480b52c0e7b6
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2026-01-16 15:00:31 +0800

    ;(

--- app/GetStarted/page.tsx
+++ app/GetStarted/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import GetStarted from '../../components/GetStarted'
+import { Suspense } from 'react';
 export default async function page() {
   return (
     <>
+    <Suspense fallback={<p>Loading...</p>}>
         <GetStarted />
+        </Suspense>
     </>
   )
 }

f151463320b92745e1ac1d2f4e878804172f34c1
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2026-01-16 13:11:27 +0800

    adding blog features

---
+++ app/PrivatePosts/PaginationControlls.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { FC } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+interface PaginationControllProps{
+    hasNextPage: boolean
+    hasPrevPage: boolean
+}
+
+export const PaginationControlls: FC<PaginationControllProps> = ({
+    hasNextPage,
+    hasPrevPage
+} ) => {
+const router = useRouter();
+const searchParams = useSearchParams();
+const page = searchParams.get('page') ?? '1';
+const per_page = searchParams.get('per_page') ?? '5';
+ return(
+    <div className='flex gap-2'>
+        <button className='btn btn-primary text-white p-1'
+        disabled={!hasPrevPage}
+        onClick={() => {
+            router.push(`pagination1/?page=${Number(page) -1}&per_page=${per_page}`)
+        }} >prev-page</button>
+        <div>
+        {page}/{Math.ceil(10/Number(per_page))}
+        </div>
+        <button className='btn btn-primary text-white p-1' 
+        disabled={!hasNextPage}
+        onClick={() => {
+            router.push(`/pagination1/?page=${Number(page) + 1}&per_page=${per_page}`)
+        }}>
+            next-page
+        </button>
+    </div>
+
+    
+ )
+}
---
+++ app/Settings/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/AppContext/AuthContext';
+export default function page() {
+    const router = useRouter();
+    const {user} = useAuth();
+    if(!user)return;
+  return (
+        <>
+            <div className="hero bg-base-200 p-4 sm:p-5 min-h-150" >
+                <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm">
+                <div className="card-body">
+                    <h2 className="card-title text-base sm:text-lg">{user.user_metadata.name}</h2>
+                    <p className="text-sm sm:text-base">{user.email}</p>
+                      <button className='btn btn-primary' onClick={() => router.replace('/PrivatePosts')}>Manage Private Posts</button>
+                    <div className="justify-end card-actions">
+                    </div>
+                </div>
+                </div>
+            </div>
+        </>
+  )
+}

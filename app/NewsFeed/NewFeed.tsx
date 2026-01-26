6f3784f6f11103eb6fb964a34062f8a08f33b8ad
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2026-01-26 16:42:24 +0800

    adding view/update function

--- app/NewsFeed/NewFeed.tsx
+++ app/NewsFeed/NewFeed.tsx
@@ -3,159 +3,71 @@ import { useSearchParams } from 'next/navigation';
 import { useFetchPostsQuery, useUpdatePost1Mutation, useDeletePost1Mutation, useDeleteAllComments1Mutation, useFetchCommentsQuery } from '@/AppRedux/Slices/postApi';
 import { PaginationControlls } from '../../components/PaginationControlls';
 import { useAuth } from '@/AppContext/AuthContext';
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import GetStarted from '../../components/GetStarted';
-import { supabase } from '@/lib/supabase/client';
 import { useRouter } from 'next/navigation';
-import { deleteAllImage } from '@/lib/utils/UploadImage';
+import { deleteAllImage, deleteImage, UploadImage } from '@/lib/utils/UploadImage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function NewsFeed() {  
-const router = useRouter();
-const [user_id, setUser_id] = useState<string|null>(null)
-const [update, setUpdate] = useState<any|null>(null);
-const [picture, setPicture] = useState<File|null>(null);
-const [screen, setScreen] = useState<"main"|"edit"|"image">("main");
-const [preview, setPreview] = useState<string|null>(null);
+  const router = useRouter();
+  const [user_id, setUser_id] = useState<string|null>(null);
   const [deletePost1] = useDeletePost1Mutation();
   const [deleteAllComments1] = useDeleteAllComments1Mutation();
-  const [updatePost1] = useUpdatePost1Mutation();
   const { data: posts = [] } = useFetchPostsQuery();
+  const {data: comments=[]}=useFetchCommentsQuery();
   const {user} = useAuth();
-  useEffect(() => {
-          if(!user)return;
-          setUser_id(user.id);
-      })
-  const getFileImages = (url:string) => {
-    const parts = url.split('/postsImages/');
-    return parts[1];
-  }
-  const deleteImageFromStorage = async(imageUrl:string) => {
-    const filePath = getFileImages(imageUrl);
-    const {error} = await supabase.storage.from('postsImages')
-                                          .remove([filePath])
-  }
   const handleRemovePost = async(post:any) => {
         try{if(post.id){await deleteAllImage(post.id)}
             await deleteAllComments1(post.id)
+            if(post.image)(await deleteImage(post.image))
             const res:any = await deletePost1(post.id)
-            
             console.log(res.message)
-        }catch(error:any){
-            console.log(error.message)
-        }}
-  const handleUpdateImage = async(file: File|null, userId:string) => {
-        if(!file) return null;
-        const ext = file.name.split('.').pop();
-        const fileName = `${userId}-${Date.now()}.${ext}`;
-        const {error} = await supabase.storage.from('postsImages')
-                                              .update(fileName, file);
-        if(error)throw error;
-        const {data} = supabase.storage.from('postsImages')
-                                              .getPublicUrl(fileName);
-        return data.publicUrl;
-  }
-  const handleUpdatePost = async(e:FormEvent<HTMLFormElement>) => {
-              e.preventDefault();
-              if(!update.title || !update.description)return; 
-              await updatePost1(update); 
-              setUpdate(null);
-              setScreen('main');
+        }catch(error:any){console.log(error.message)}
     }
-  const EditImagePost = async(e:FormEvent<HTMLFormElement>) => {
-              e.preventDefault();
-              if(!update.title || !update.description || !user_id)return;  
-              const image = await handleUpdateImage(picture, user_id)
-              if(update.image){await deleteImageFromStorage(update.image)}
-              await updatePost1({...update, image: image}); 
-              setUpdate(null);
-              setPreview(null);
-              setScreen('main');
-  }
-
   const searchParams = useSearchParams();
   const page = Number(searchParams.get('page') ?? 1);
   const perPage = Number(searchParams.get('per_page') ?? 5);
   const start = (page - 1) * perPage;
   const end = start + perPage;
+    const post = posts.filter((post)=> post.Public === 'public');
   const entries = posts.slice(start, end);
   return (
-    <>
-    {screen==='main' && update === null &&(
-        <div className="flex flex-col gap-2 items-center p-5">
-         {entries && user ? (   
+    <>{user ? (
+        <div className="flex flex-col gap-2 items-center p-10">
+         {entries && user && (   
             entries.filter((post: any) => post.Public === 'public')
             .map((post) => (
             <div key={post.id} className="hero bg-base-200 p-5 ">
-                    <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm cursor-pointer" >
+                    <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm " >
                         <div className="card-body">
+                            <div className="flex items-center gap-2">
+                                <div className="btn btn-soft btn-primary btn-circle"><FontAwesomeIcon icon={faUser} size="sm" /></div>
+                                <p>{post.author}</p>
+                            </div>
+                            {post.image &&(<img src={post.image} alt={post.image} />)}
                             <h2 className="card-title">{post.title}</h2>
-                            {post.image &&(<img src={post.image} alt={post.image} onClick={(e)=>{setScreen('image'); setUpdate(post)}} className='cursor-pointer' />)}
                             <p>{post.description}</p>
-                            <p>{post.author}</p>
                             <div className="justify-end card-actions">
                                 <button className='btn btn-accent' onClick ={() => router.push(`/Post/ViewPosts/${post.id}`)}>View</button>
                             {post.user_id === user.id &&(
                             <>
-                                <button className="btn btn-primary" onClick={() => {setUpdate(post); setScreen('edit'); setPicture(null);}}>Update</button>
+                                {/* <button className="btn btn-primary" onClick={() => {setUpdate(post); setScreen('edit'); setPicture(null);}}>Update</button> */}
+                                <button className="btn btn-primary" onClick={() => {router.push(`/Post/UpdatePosts/${post.id}`)}}>Update</button>
                                 <button className="btn btn-error" onClick={() => handleRemovePost(post)} >Delete</button>
                             </>)}
                             </div>
                         </div>
+                        <p className='p-3'>{comments.filter(c=>c.post_id === post.id).length} comment</p>
                         <button className="btn btn-block" onClick ={() => router.push(`/Post/ViewPosts/${post.id}`)}>Comment</button>
                     </div>
             </div>
-            ))) : (<>{!user ? <GetStarted/>:<p>No posts found.</p>}</>)}
-            <PaginationControlls hasNextPage={end < posts.length} hasPrevPage={start > 0} />
-        </div>)}
-
-        {screen === 'edit' && update !==null && (
-            <div className='w-full min-h-screen sticky top-[0px] left-[0px] bg-neutral-100 flex justify-center items-center p-8'>
-                  <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm ">
-                        <form className="card-body" onSubmit={handleUpdatePost}>
-                            <label htmlFor="" className='mr-2'>
-                            <input type="radio" name="radio-1" className="radio mr-1" value={'public'} checked={update.Public === 'public'} onChange={(e) => setUpdate({...update, Public: e.target.value,})} />
-                            public</label>
-                            <label htmlFor="" className='mr-2'>
-                            <input type="radio" name="radio-1" className="radio mr-1" value={'private'} checked={update.Public === 'private'} onChange={(e) => setUpdate({...update, Public: e.target.value,})}/>
-                            private</label>
-                            <label>Image</label>
-                            {update.image ? (<img src={update.image} alt={update.image} onClick={(e)=>{setScreen('image'); setUpdate(update)}} className='cursor-pointer'  />)
-                                    :<button onClick={(e)=>{setScreen('image'); setUpdate(update)}} className='w-full btn btn-primary'>Add Image</button>}
-                            <label htmlFor="">Title</label>
-                            <input className='input w-full' type="text" value={update.title} onChange={(e) => setUpdate({...update, title: e.target.value,})} placeholder='title'/>
-                            <label htmlFor="">Description</label>
-                            <textarea className='textarea w-full' rows={3} value={update.description} onChange={(e) => setUpdate({...update, description: e.target.value,})} placeholder='description'/>
-                            <div className="justify-end card-actions">
-                            
-                            <button className='btn btn-error' onClick={() => {setUpdate(null); setScreen('main'); setPreview(null)}} >Cancel</button>
-                            <button className="btn btn-primary " >Update</button>
-
-                            </div>
-                        </form>
-                    </div>
-            </div>)}
-
-        {screen === 'image' && update !==null && (
-            <div className='w-full min-h-screen sticky top-[0px] left-[0px] bg-neutral-100 flex justify-center items-center p-8'>
-                  <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm ">
-                        <form className="card-body" onSubmit={EditImagePost}>
-                            <label>Image</label>
-                            {preview === null ? update.image && (<img src={update.image} alt={update.image} />) : <img src={preview} /> }
-                            <input onChange={(e)=>{
-                                    if(e.target.files && e.target.files[0]){
-                                        setPicture(e.target.files[0])
-                                        setPreview(URL.createObjectURL(e.target.files[0]))
-                                    }}}
-                                   type='file' 
-                                   accept='image/*'
-                                   className="file-input file-input-bordered file-input-primary w-full file-input-sm" 
-                            />
-                            <button className='btn btn-error' onClick={() => {setUpdate(null); setScreen('main'); setPreview(null)}} >Cancel</button>
-                            <button className="btn btn-primary " >Update</button>
-                        </form>
-                    </div>
-            </div>)}
-
+            ))) }
+            <PaginationControlls hasNextPage={end < post.length} hasPrevPage={start > 0} />
+        </div>
+    ): <GetStarted/>}
     </>    
   );
 }
--- app/PrivatePosts/PrivatePosts.tsx
+++ app/PrivatePosts/PrivatePosts.tsx
@@ -1,139 +1,71 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
-import { useFetchPostsQuery, useUpdatePost1Mutation, useDeletePost1Mutation } from '@/AppRedux/Slices/postApi';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useFetchPostsQuery, useUpdatePost1Mutation, useDeletePost1Mutation, useFetchCommentsQuery } from '@/AppRedux/Slices/postApi';
 import { PaginationControlls1 } from '../../components/PaginationControlls1';
 import { useAuth } from '@/AppContext/AuthContext';
-import { FormEvent, useEffect, useState } from 'react';
 import GetStarted from '../../components/GetStarted';
-import { supabase } from '@/lib/supabase/client';
+import { deleteImage } from '@/lib/utils/UploadImage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 export default function PrivatePosts() {  
-const [user_id, setUser_id] = useState<string|null>(null)
-const [update, setUpdate] = useState<any|null>(null);
-const [preview, setPreview] = useState<string|null>(null);
-const [picture, setPicture] = useState<File|null>(null);
-const [title, setTitle] = useState('');
-const [Public, setPublic] = useState('');
-const [description, setDescription] = useState('');
-  const [deletePost1] = useDeletePost1Mutation();
-  const [updatePost1] = useUpdatePost1Mutation();
-  useEffect(() => {
-            if(!user)return;
-            setUser_id(user.id);
-        })
-  const getFileImages = (url:string) => {
-    const parts = url.split('/postsImages/');
-    return parts[1];
-  }
-  const deleteImageFromStorage = async(imageUrl:string) => {
-    const filePath = getFileImages(imageUrl);
-    const {error} = await supabase.storage.from('postsImages')
-                                          .remove([filePath])
-  }
+const {user} = useAuth();
+const router = useRouter();
+const [deletePost1] = useDeletePost1Mutation();
+const { data: posts = [] } = useFetchPostsQuery();
+const {data:comments=[]}=useFetchCommentsQuery();
   const handleRemovePost = async(post:any) => {
-        try{
-            const res:any = await deletePost1(post.id);
-            if(post.image){await deleteImageFromStorage(post.image)}
+        try{const res:any = await deletePost1(post.id);
+            if(post.image){await deleteImage(post.image)}
             console.log(res.message)
         }catch(error:any){
             console.log(error.message)
         }}
-  const updateImagePost = async(file: File|null, userId:string) => {
-    if(!file) return null;
-        const ext = file.name.split('.').pop();
-        const fileName = `${userId}-${Date.now()}.${ext}`;
 
-        const {error} = await supabase.storage.from('postsImages')
-                                              .update(fileName, file);
-        if(error) throw error;
-        const {data} = supabase.storage.from('postsImages') 
-                                             .getPublicUrl(fileName);
-        return data.publicUrl;
-  }
-  const handleUpdatePost = async(e:FormEvent<HTMLFormElement>) => {
-              e.preventDefault();
-              if(title === update.title || description === update.description || !user_id)return;  
-              const image = await updateImagePost(picture, user_id);
-              if(update.image){await deleteImageFromStorage(update.image)}
-              await updatePost1({...update, image: image}); 
-              setUpdate(null);
-              setPreview(null)
-    }
   const searchParams = useSearchParams();
-  const {user} = useAuth();
   const page = Number(searchParams.get('page') ?? 1);
   const perPage = Number(searchParams.get('per_page') ?? 5);
-
-  const { data: posts = [] } = useFetchPostsQuery();
-
   const start = (page - 1) * perPage;
   const end = start + perPage;
-  const entries = posts.slice(start, end);
-  if(!user)return;
+  const post = posts.filter((post)=> post.user_id === user?.id && post.Public === 'private');
+  const entries = post.slice(start, end);
+  
   return (
     <>
-    {update === null && (
-        <div className="flex flex-col gap-2 items-center p-5">
-         {entries && user ? (   
+    {user ? (
+        <div className="flex flex-col gap-2 items-center p-10">
+         {entries && user && (   
             entries.filter((post: any) => post.user_id === user.id && post.Public === 'private')
             .map((post) => (
-            <div key={post.id} className="hero bg-base-200 p-4 sm:p-5">
+                <div key={post.id} className="hero bg-base-200 p-4 sm:p-5">
                     <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm">
                         <div className="card-body">
-                            <h2 className="card-title">{post.title}</h2>
+                            <div className="flex items-center gap-2">
+                                <div className="btn btn-soft btn-primary btn-circle"><FontAwesomeIcon icon={faUser} size="sm" /></div>
+                                <p>{post.author}</p>
+                            </div>
                             {post.image &&(<img src={post.image} />)}
+                            <h2 className="card-title">{post.title}</h2>
                             <p>{post.description}</p>
-                            <p>{post.author}</p>
                             <div className="justify-end card-actions">
+                                <button className='btn btn-accent' onClick ={() => router.push(`/Post/ViewPosts/${post.id}`)}>View</button>
                             {post.user_id === user.id &&(
                             <>
-                                <button className="btn btn-primary" onClick={() => setUpdate(post)}>Update</button>
+                                <button className="btn btn-primary" onClick={() => router.push(`/Post/UpdatePosts/${post.id}`)}>Update</button>
                                 <button className="btn btn-error" onClick={() => handleRemovePost(post)} >Delete</button>
                             </>)}
                             </div>
                         </div>
+                        <p className='p-3'>{comments.filter(c=>c.post_id === post.id).length} comment</p>
+                        <button className="btn btn-block" onClick ={() => router.push(`/Post/ViewPosts/${post.id}`)}>Comment</button>
                     </div>
-            </div>
-            ))) : (<>{!user ? <GetStarted/>:<p>No posts found.</p>}</>)}
-            <PaginationControlls1 hasNextPage={end < posts.length} hasPrevPage={start > 0} />
-        </div>)}
-
-        {update !== null && (
-            <div className='w-full min-h-screen top-[0px] left-[0px] bg-neutral-100 flex justify-center items-center p-8'>
-                  <div className="card w-full max-w-sm sm:max-w-md bg-base-100 card-xl shadow-sm ">
-                        <form className="card-body" onSubmit={handleUpdatePost}>
-                            <label htmlFor="" className='mr-2'>
-                            <input type="radio" name="radio-1" className="radio mr-1" value={'public'} checked={update.Public === 'public'} onChange={(e) => setUpdate({...update, Public: e.target.value,})} />
-                            public</label>
-                            <label htmlFor="" className='mr-2'>
-                            <input type="radio" name="radio-1" className="radio mr-1" value={'private'} checked={update.Public === 'private'} onChange={(e) => setUpdate({...update, Public: e.target.value,})}/>
-                            private</label>
-                            <label>Image</label>
-                            {preview === null ? update.image && (<img src={update.image} />):<img src={preview} />}
-                            <input  onChange={(e) => {
-                                        if(e.target.files && e.target.files[0]){
-                                            setPicture(e.target.files[0]);
-                                            setPreview(URL.createObjectURL(e.target.files[0]));
-                                        }
-                                    }} 
-                                    type="file"
-                                    accept='image/*'
-                                    className="file-input file-input-bordered file-input-primary w-full file-input-sm"                                                    
-                            />
-                            <label htmlFor="">Title</label>
-                            <input className='input w-full' type="text" value={update.title} onChange={(e) => setUpdate({...update, title: e.target.value,})} placeholder='title'/>
-                            <label htmlFor="">Description</label>
-                            <textarea className='textarea w-full' rows={3} value={update.description} onChange={(e) => setUpdate({...update, description: e.target.value,})} placeholder='description'/>
-                            <div className="justify-end card-actions">
-                            
-                            <button className='btn btn-error' onClick={() => {setUpdate(null); setPreview(null)}} >Cancel</button>
-                            <button className="btn btn-primary " >Update</button>
-
-                            </div>
-                        </form>
-                    </div>
-            </div>)}
-
+                </div>
+            ))) }
+            <PaginationControlls1 hasNextPage={end < post.length} hasPrevPage={start > 0} />
+        </div>
+    ): <GetStarted/>}
     </>    
   );
 }
+

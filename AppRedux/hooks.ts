bdf22fcb5b587cc66e5d8c6c45dd21b2b1f97803
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2026-02-01 14:16:41 +0800

    cleaning files ;)

--- AppRedux/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { type TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
-import { AppDispatch, RootState } from './store';
-
-export const useAppDispatch = () => useDispatch<AppDispatch>;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
